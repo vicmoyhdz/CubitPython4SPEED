4e0b4d819b2c07d8fffe13549e7f6fa140acb233
Victor Hernández <133799102+vicmoyhdz@users.noreply.github.com>
2023-11-03 11:11:24 +0100

    Add files via upload

--- ExampleRefinement.py
+++ ExampleRefinement.py
@@ -2,11 +2,11 @@ import cubit
 cubit.init(['cubit','-nojournal'])
 cubit.reset()
 
-cubit.cmd('cd \'C:\\Users\\vmh5\\Documents\\CubitPython\'')
+cubit.cmd('cd \'C:\\Users\\vmh5\\Documents\\CubitPython4SPEED\'')
 #configuration file f with the option curve_refinement=True :
-f="Files\Refinement.cfg"
+f="Files\TopoExampleRefinement.cfg"
 # file is the merged unrefined mesh:
-file="C:/vol_merged.cub5"
+file="C:/Users/vmh5/Documents/CubitPython4SPEED/Example outputs/TopoExample_vol_merged.cub5"
 #here you load the mesh in Cubit:
 cubit.cmd('import cubit "' + file + '"')
 cubit.cmd('set echo off')
